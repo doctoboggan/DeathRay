e5a23c738ece5f57c2306830784a5400f96cf641
vanderteam <anas.alfuntukh@vanderbilt.edu>
2012-02-13 07:17:02 -0600

    Update test-programs/currentAC.py

--- test-programs/currentAC.py
+++ test-programs/currentAC.py
@@ -1,11 +1,11 @@
 # Name: Current AC Reader
 # Made by: Anas Alfuntukh
 # Date: 12/02/12  (MM/DD/YY)
-# Goal: This moduel suppose to be able to return the Current AC (with frequency) value from the given devices.
+# Goal: This moduel suppose to be able to return the AC current (with frequency) value from the given devices.
 # Devcies:  1) "hp34401a" 
 # Modifiers: 
 # SCPI command: meas:curr:ac?
-# Result: two floats. One of them is current. And, the other one is the frequency. 
+# Result: two floats. One of them is AC current. And, the other one is the frequency. 
 
 import libgpib
 
@@ -36,7 +36,7 @@ class currentAC:
 
   def get(self):		
     """
-    The main SCPI commands, where the current AC value is !!
+    The main SCPI commands, where the AC current value is !!
     """
     m = eval('libgpib.'+ self.name_of_device+'(host="'+self.ip_id+'", device="'+self.gpib_id+'")')   
     z , c , currfix = m.transaction('meas:curr:ac?')
