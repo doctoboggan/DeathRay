c612992ce4ae23ef53379bb2f2fc7efc31d9e479
unknown <zainoln@zainoln-PC.(none)>
2012-02-17 11:33:38 -0600

    Trigger mode

---
+++ test-programs/TrigMode.py
@@ -0,0 +1,64 @@
+# Name: Sets the trigger mode for given channel of given devices.
+# Made by: Nadiah Zainol Abidin
+# Date: 02/17/12  (MM/DD/YY)
+# Goal: sets the trigger mode of the oscilloscope, 
+# Devices:  1) dso
+# Modifiers:  None 
+# SCPI command: Command Syntax :TRIGger:MODE <mode>
+# <mode> ::= {EDGE | GLITch | PATTern | CAN | DURation | I2S |IIC
+# | EBURst | LIN | M1553| SEQuence | SPI | TV | UART| USB | FLEXray}
+# Result: One string. it notifies the osc is set to the trigger mode requested  
+
+import libgpib
+
+class CcurrentDC:		
+  """
+  This class sets the trigger mode of the given device. 
+  """
+
+  def __init__(self, IPad, Gpibad, namdev, trigmode): 
+
+    self.ip_id = IPad
+    self.gpib_id = Gpibad
+    self.name_of_device = namdev
+    self.rightDevice = ['dso']
+    self.trigMode_for_dso = ['EDGE', 'GLITch', 'PATTern', 'CAN', 'DURation', 'I2S', 'IIC', 'EBURst', 'LIN', 'M1553', 'SEQuence', 'SPI', 'TV', 'UART', 'USB', 'FLEXray']
+    self.trigMode = trigmode
+
+  def check(self):
+    """
+    To check if the given device will work with CCurrentDC function (to avoid module from crashing).
+    Also, it checks if the specified channel of certain device matchs the data base of the names of channels in that device        
+    """
+    if self.name_of_device not in self.rightDevice:
+      return False
+    if self.name_of_device is 'dso':
+      if self.trigMode not in self.trigMode_for_dso:
+        return False
+
+    return True
+  
+
+
+  def get(self):		
+    """
+    The main SCPI command. It sets the oscilloscope in the trigger mode requested
+    """
+    m = eval('libgpib.'+ self.name_of_device+'(host="'+self.ip_id+'", device="'+self.gpib_id+'")')   
+    z , c , x = m.transaction('TRIG:MODE '+self.trigMode)
+    m.disconnect() 
+    return self.trigMode+' has been selected.'
+
+''''
+page 485
+:TRIGger:MODE
+(see page 798)
+Command Syntax :TRIGger:MODE <mode>
+<mode> ::= {EDGE | GLITch | PATTern | CAN | DURation | I2S |IIC
+| EBURst | LIN | M1553| SEQuence | SPI | TV | UART
+| USB | FLEXray}
+The :TRIGger:MODE command selects the trigger mode (trigger type).
+Query Syntax :TRIGger:MODE?
+The :TRIGger:MODE? query returns the current trigger mode. If the
+:TIMebase:MODE is ROLL or XY, the query returns "NONE".
+
