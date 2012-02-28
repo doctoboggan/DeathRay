4dcfd3fad851fab03e11e6eae585fdd05ea59089
Jack Minardi <jack@minardi.org>
2012-02-27 22:52:00 -0600

    notes update

--- test-gui/modules/__init__.py
+++
@@ -1,9 +0,0 @@
-from voltageDC import voltageDC
-from voltageAC import voltageAC
-from currentDC import currentDC
-
-command = {
-    'voltageDC': voltageDC,
-    'voltageAC': voltageAC,
-    'currentDC': currentDC
-    }
