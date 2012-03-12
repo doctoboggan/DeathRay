8a132e29a219dff43592e7743da802fefc0063a7
nadiahhusseini <nadiah.husseini.zainol.abidin@vanderbilt.edu>
2012-03-12 13:26:59 -0500

    partly done converting

--- test-programs/ImpedanceChannel.py
+++ test-programs/ImpedanceChannel.py
@@ -5,11 +5,15 @@
 # Devices:  1) "dso6032a" 
 # Modifiers:  None 
 
-
 import libgpib
 
-class ImpedanceChannel:		
+class ImpedanceChannel(data_acquisition.vxi_11.vxi_11_connection,data_acquisition.gpib_utilities.gpib_device):		
   """
+This class selects the input impedance setting for the specified analog channel. The legal values for this command are
+ONEMeg (1 MΩ) and FIFTy (50Ω),please use rightdevice' function.
+We are feeding the class with vxi_11.vxi_11_connection and gpib_utilities.gpib_device from data_acquisition library 
+  
+
 The :CHANnel<n>:IMPedance command selects the input impedance setting
 for the specified analog channel. The legal values for this command are
 ONEMeg (1 MΩ) and FIFTy (50Ω).
@@ -32,12 +36,64 @@ fixed at ONEMeg (1 MΩ).
     self.name_of_device = namdev
     self.rightDevice = ['dso6032a']
     self.channel = channel
-    self.typeChannel = ['1', '2', '3', '4']
     self.impedance = impedance
+    self.typeChannel = ['1', '2', '3', '4', ' ']
     self.typeImpedance = ['ONEMeg', '1Meg', '1M', '1MEG', 'ONEMEG', 'ONEM', 'FIFTY', '50OHM', 'FIFT', 'fifty', 'fiftyohm', 'fiftyOhm', '50']
-    self.ONEMegImpedance = ['ONEMeg', '1Meg', '1M', '1MEG', 'ONEMEG', 'ONEM']
-    self.50OhmImpedance = ['FIFTY', '50OHM', 'FIFT', 'fifty', 'fiftyohm', 'fiftyOhm', '50']
+    self.ONEMegImpedance = ['ONEMeg', '1Meg', '1M', '1MEG', 'ONEMEG', 'ONEM', '1000']
+    self.FiftyOhmImpedance = ['FIFTY', '50OHM', 'FIFT', 'fifty', 'fiftyohm', 'fiftyOhm', '50']
+    rise_on_error = 0
+    data_acquisition.vxi_11.vxi_11_connection.__init__(self,host=IPad,device=Gpibad,raise_on_err=rise_on_error,timeout=timeout,device_name=namdev)  #here we are feeding the data_acquisition library
 
+  
+  def check(self):
+    """
+    To check if the given device will work with ImpedanceChannel (avoiding issues).
+    Also, it makes sure that the input channels do exist (to aviod conflicts). 
+    ALso, we take care of time-out minimum duration (to aviod run out of time).
+    Also, we remind the user that the input channel is required for the given device. 
+    """
+    
+    if self.name_of_device in self.rightDevice:
+
+      if self.timeout >= 2500:      # hardcoded. Also, the number was choosen after several testing.
+
+        if self.name_of_device == 'dso6032a':
+
+          if self.channel not in self.typeChannel :      # cor channel checking. We have to do this with each and every channelly device!!
+            print "chosen channel does not exist !!"     # For debug purpose
+            return False, 'c'
+
+          else:
+            if self.channel != '':
+              print "The device does not have any channel. So, your input channel will be ignored."     # To remind the user about his/her mistake of entering channel, where the device does not have. 
+              return True
+         
+         else:
+            return True
+            
+          
+            if self.impedance not in self.typeImpedance
+              print "The impedance selected is not valid. Please enter only ONEM (1Meg Ohms) or FIFT (fifty Ohms)"
+              return False, "i"
+              
+            else: 
+              if self.impedance is in self.ONEMegImpedance:
+                return self.impedance = 'ONEM' ##anas, please check this line if it is valid or not
+    
+            else:
+              if self.impedance is in self.FiftyOhmImpedance:
+                return self.impedance = 'FIFT' ##anas, please check if this is valid or not
+      
+      else:
+        print "The time-out is too short"   # For debug purpose
+        return False, 'o'
+
+    else:
+      print "the device is not in data base"    # For debug purpose
+      return False, 'x'
+
+
+  
   def check(self):
     """
         
@@ -48,10 +104,7 @@ fixed at ONEMeg (1 MΩ).
       return False
     if self.impedance is not in self.typeImpedance:
       return False
-    if self.impedance is in self.ONEMegImpedance:
-      return self.impedance = 'ONEM' ##please check this line if it is valid or not
-    if self.impedance is in self.50OhmImpedance:
-      return self.impedance = 'FIFT'
+    
     return True
 
 
