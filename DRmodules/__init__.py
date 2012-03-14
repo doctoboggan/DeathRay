9b2a7faa565cf1a53cab412df2f4ecb6bcc67698
Jack Minardi <jack@minardi.org>
2012-03-14 16:51:26 -0500

    getTrigMode updates

--- DRmodules/__init__.py
+++ DRmodules/__init__.py
@@ -7,6 +7,7 @@ from getIDN import getIDN
 from setvoltageDC import setvoltageDC
 from getcurrentAC import getcurrentAC
 from setAcqType import setAcqType
+from getTrigMode import getTrigMode
 command = {
     'getvoltageDC': getvoltageDC,
     'getvoltageAC': getvoltageAC,
@@ -16,7 +17,8 @@ command = {
     'getIDN': getIDN,
     'setvoltageDC': setvoltageDC,
     'getcurrentAC': getcurrentAC,
-    'setAcqType': setAcqType
+    'setAcqType': setAcqType,
+    'getTrigMode': getTrigMode
     }
 # never ever have an empty line inside this file !! (the program will crash!!)
 # because of the "command" dic.We have to get use to how to import stuff from DRmodules (not the same way as DRscripts and other stuff because of different in __init__ method )
