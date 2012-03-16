aeebd33c1978b80054cca9d82929bf7e98c0fe2d
computer <computer@computer.(none)>
2012-03-15 19:10:54 -0500

    still cleaning

---
+++ setup.sh
@@ -0,0 +1,6 @@
+python /interfaces/lib_m.py install 
+#python main.py (I thinkwe need to add some kind of steps)
+# Also, we need creat a shortcut for the program in user Desktop.
+# we can not doabove until we have one main file (contain FPGA and GPIB). 
+
+
