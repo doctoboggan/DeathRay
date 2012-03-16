279c618810f8212f4db5c8b5b58bae0ddee34fb9
computer <computer@computer.(none)>
2012-03-15 21:27:46 -0500

    fix setup

--- interfaces/lib_m.py
+++ interfaces/lib_m.py
@@ -7,12 +7,12 @@ Created on Mar 12, 2012
 from distutils.core import setup
 
 setup(name='DeathRay',
-      version='0.13',
+      version='0.15',
       description='A GUI program with its plugin for controlling and analyzing FPGA and Gpib devices',
       author='Jack Minardi, Nadiah Husseini Zainol Abidin and Anas Khalid Alfuntukh',
       author_email='jack@minardi.org, nadiah.husseini.zainol.abidin@vanderbilt.edu and anas.alfuntukh@vanderbilt.edu',
-      packages=['DRmodules'],
-      package_dir={'DRmodules': 'DRmodules'},
+      packages=['gpib_commands'],
+      package_dir={'gpib_commands': './gpib_commands'},   # Here, the path only works when the user run setup file. If the user tries to install it from here, it will fail.
       requires=['data_acquisition','numpy'],
       provides=['DeathRay']
      )
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-python /interfaces/lib_m.py install 
+python ./interfaces/lib_m.py install 
 #python main.py (I thinkwe need to add some kind of steps)
 # Also, we need creat a shortcut for the program in user Desktop.
 # we can not doabove until we have one main file (contain FPGA and GPIB). 
