a146f60eaaa87aa711cef914d0af08c8cfcd0ea4
Jack Minardi <jack@minardi.org>
2012-03-25 15:48:57 -0500

    Device Control updates

--- DeviceControl.py
+++ DeviceControl.py
@@ -28,20 +28,26 @@ class DeviceControl(QtGui.QMainWindow):
     
     #instance variables
     self.argDict = {}
+    self.savedCommands = []
     self.commands = {'set':{}, 'get':{}}
     self.setOrGet = 'set'
 
-    self.radioToggled()
-
-
   def initUI(self):
     #Connect the signals and slots
+
+    #list widgets selected
     self.connect(self.ui.listWidgetDevices, QtCore.SIGNAL('itemSelectionChanged()'), self.deviceSelected)
     self.connect(self.ui.listWidgetCommands, QtCore.SIGNAL('itemSelectionChanged()'), self.commandSelected)
+    self.connect(self.ui.listWidgetSavedCommands, QtCore.SIGNAL('itemSelectionChanged()'),
+        self.saveCommandSelected)
+    #buttons
     self.connect(self.ui.pushButtonExecute, QtCore.SIGNAL('clicked()'), self.getClicked)
     self.connect(self.ui.pushButtonFindDevices, QtCore.SIGNAL('clicked()'), self.findDevicesClicked)
     self.connect(self.ui.radioButtonSet, QtCore.SIGNAL('clicked()'), self.radioToggled)
     self.connect(self.ui.radioButtonGet, QtCore.SIGNAL('clicked()'), self.radioToggled)
+    self.connect(self.ui.pushButtonSave, QtCore.SIGNAL('clicked()'), self.saveClicked)
+    self.connect(self.ui.pushButtonDelete, QtCore.SIGNAL('clicked()'), self.deleteClicked)
+    self.connect(self.ui.pushButtonClear, QtCore.SIGNAL('clicked()'), self.clearClicked)
     
     #set the argument boxes invisible at first
     self.ui.labelArg1.setVisible(False)
@@ -55,12 +61,21 @@ class DeviceControl(QtGui.QMainWindow):
     self.ui.labelArg5.setVisible(False)
     self.ui.lineEditArg5.setVisible(False)
     
+    #Set sizes, alignments, and initial checked states
     self.ui.verticalLayoutArguments.setAlignment(Qt.Qt.AlignTop)
     self.ui.splitter.setSizes([200,200,200])
+    self.ui.splitter_vert.setSizes([200,100])
     self.ui.radioButtonSet.setChecked(True)
 
 
+
+############################
+#### DRAWING METHODS
+############################
+
   def updateDeviceList(self):
+    '''Draw the found devices to the device list widget
+    '''
     self.ui.listWidgetDevices.clear()
     for deviceName in self.deviceList:
       listItem = QtGui.QListWidgetItem(self.ui.listWidgetDevices)
@@ -68,13 +83,96 @@ class DeviceControl(QtGui.QMainWindow):
 
 
   def updateCommands(self, deviceName):
+    '''Draw the found commands to the commands list widget
+    '''
     self.ui.listWidgetCommands.clear()
     for command in self.commands[self.setOrGet][str(deviceName).lower()]:
       listItem = QtGui.QListWidgetItem(self.ui.listWidgetCommands)
       listItem.setText(command)
 
+  
+  def updateSavedCommands(self, text):
+    '''Draw the saved command to the saved commands list widget
+    '''
+    listItem = QtGui.QListWidgetItem(self.ui.listWidgetSavedCommands)
+    listItem.setText(text)
+
+
+  def showArgBoxes(self, numOfArgs):
+    '''sets the correct amount of text boxes visible
+    '''
+    self.ui.labelArg1.setVisible(numOfArgs>0)
+    self.ui.lineEditArg1.setVisible(numOfArgs>0)
+    self.ui.labelArg2.setVisible(numOfArgs>1)
+    self.ui.lineEditArg2.setVisible(numOfArgs>1)
+    self.ui.labelArg3.setVisible(numOfArgs>2)
+    self.ui.lineEditArg3.setVisible(numOfArgs>2)
+    self.ui.labelArg4.setVisible(numOfArgs>3)
+    self.ui.lineEditArg4.setVisible(numOfArgs>3)
+    self.ui.labelArg5.setVisible(numOfArgs>4)
+    self.ui.lineEditArg5.setVisible(numOfArgs>4)
+
+
+
+##############################
+#### BUTTON CLICKED METHODS
+##############################
+ 
+  def findDevicesClicked(self):
+    '''Find the attached devices, call findArguments, then update the device display widget
+    '''
+    self.IP = str(self.ui.lineEditIP.text())
+    #self.deviceList, self.GPIBlist = utils.getAttachedDevices(self.IP, 30).fix()
+    self.deviceList = ['DSO6032A', 'E3631A', '34401A']
+    self.GPIBlist = ['gpib0,07', 'gpib0,10', 'gpib0,22']
+
+    self.findArguments()
+    self.updateDeviceList()
+
+
+  def getClicked(self):
+    '''
+    this method is called whenever the get button is clicked. It makes the call to
+    the correct command script and feeds it the arguments it needs
+    '''
+    command, args, kwargs = self.returnCurrentCommand()
+    commandObject = gpib_commands.command[command](*args, **kwargs)
+
+    result = str(commandObject.do())
+    self.ui.lineEditResult.setText(result)
+
+
+  def saveClicked(self):
+    command, args, kwargs = self.returnCurrentCommand()
+    self.savedCommands.append((command, args, kwargs))
+
+    text = str(args[2])+' -> '+str(command) + str(kwargs)
+    self.updateSavedCommands(text)
+
+  def deleteClicked(self):
+    pass
+
+
+  def clearClicked(self):
+    self.savedCommands = []
+    self.ui.listWidgetSavedCommands.clear()
+
+
+  def radioToggled(self):
+    if self.ui.radioButtonSet.isChecked():
+      self.setOrGet = 'set'
+    else:
+      self.setOrGet = 'get'
+
+
+
+########################
+#### MISC SLOTS
+########################
 
   def deviceSelected(self):
+    '''This method is called whenever a device is selected. It simply calls update commands
+    '''
     self.showArgBoxes(0)
     deviceName = self.ui.listWidgetDevices.currentItem().text()
     self.updateCommands(deviceName)
@@ -84,6 +182,7 @@ class DeviceControl(QtGui.QMainWindow):
     '''This method updates the argument view to display the relevent textboxes and labels
     '''
     self.ui.pushButtonExecute.setEnabled(True)
+    self.ui.pushButtonSave.setEnabled(True)
     commandName = str(self.ui.listWidgetCommands.currentItem().text())
     args = self.argDict[commandName].keys()
     values = self.argDict[commandName].values()
@@ -109,73 +208,14 @@ class DeviceControl(QtGui.QMainWindow):
       self.ui.lineEditArg5.setText(values[4])
 
 
-  def showArgBoxes(self, numOfArgs):
-    #set the correct amount of text boxes visible
-    self.ui.labelArg1.setVisible(numOfArgs>0)
-    self.ui.lineEditArg1.setVisible(numOfArgs>0)
-    self.ui.labelArg2.setVisible(numOfArgs>1)
-    self.ui.lineEditArg2.setVisible(numOfArgs>1)
-    self.ui.labelArg3.setVisible(numOfArgs>2)
-    self.ui.lineEditArg3.setVisible(numOfArgs>2)
-    self.ui.labelArg4.setVisible(numOfArgs>3)
-    self.ui.lineEditArg4.setVisible(numOfArgs>3)
-    self.ui.labelArg5.setVisible(numOfArgs>4)
-    self.ui.lineEditArg5.setVisible(numOfArgs>4)
+  def saveCommandSelected(self):
+    pass
 
 
-  def radioToggled(self):
-    if self.ui.radioButtonSet.isChecked():
-      self.setOrGet = 'set'
-    else:
-      self.setOrGet = 'get'
 
-
-  def findDevicesClicked(self):
-    self.IP = str(self.ui.lineEditIP.text())
-    #self.deviceList, self.GPIBlist = utils.getAttachedDevices(self.IP, 30).fix()
-    self.deviceList = ['DSO6032A', 'E3631A', '34401A']
-    self.GPIBlist = ['gpib0,07', 'gpib0,10', 'gpib0,22']
-
-    self.findArguments()
-    self.updateDeviceList()
-
-
-  def getClicked(self):
-    '''
-    this method is called whenever the get button is clicked. It makes the call to
-    the correct command script and feeds it the arguments it needs
-    '''
-    kwargs={}
-    if self.numOfArgs > 0:
-      arg = str(self.ui.labelArg1.text()[:-1])
-      value = str(self.ui.lineEditArg1.text())
-      kwargs[arg] = value
-    if self.numOfArgs > 1:
-      arg = str(self.ui.labelArg2.text()[:-1])
-      value = str(self.ui.lineEditArg2.text())
-      kwargs[arg] = value
-    if self.numOfArgs > 2:
-      arg = str(self.ui.labelArg3.text()[:-1])
-      value = str(self.ui.lineEditArg3.text())
-      kwargs[arg] = value
-    if self.numOfArgs > 3:
-      arg = str(self.ui.labelArg4.text()[:-1])
-      value = str(self.ui.lineEditArg4.text())
-      kwargs[arg] = value
-    if self.numOfArgs > 4:
-      arg = str(self.ui.labelArg5.text()[:-1])
-      value = str(self.ui.lineEditArg5.text())
-      kwargs[arg] = value
-
-    command = str(self.ui.listWidgetCommands.currentItem().text())
-    device = str(self.ui.listWidgetDevices.currentItem().text())
-    GPIB = self.GPIBlist[self.deviceList.index(device)]
-    result = str(gpib_commands.command[command](self.IP, GPIB, device.lower(), **kwargs).do())
-    self.ui.lineEditResult.setText(result)
-
-    #update the commands arg dict to next time the most recently applied value is filled in
-    self.argDict[command].update(kwargs)
-
+##########################
+#### HELPER METHODS
+##########################
 
   def findArguments(self):
     '''
@@ -213,6 +253,54 @@ class DeviceControl(QtGui.QMainWindow):
                 self.commands['get'][device].append(commandName)
               else:
                 self.commands['get'][device] = [commandName]
+
+
+  def returnCurrentCommand(self):
+    '''
+    this method returns a tuple consisting of (command-name, args, kwargs) all of which are needed
+    to instantiate a command object. To create a new command object with the return value of this
+    method, do the following:
+
+    command, args, kwargs = self.returnCurrentCommand()
+    commandObject = gpib_commands.command[command](*args, **kwargs)
+
+    To execute the command, call the .do() method on the commandObject, eg:
+    commandObject.do()
+    '''
+
+    #biuld a dict of kwargs that consists of the extra arguments for any given command
+    kwargs={}
+    if self.numOfArgs > 0:
+      arg = str(self.ui.labelArg1.text()[:-1])
+      value = str(self.ui.lineEditArg1.text())
+      kwargs[arg] = value
+    if self.numOfArgs > 1:
+      arg = str(self.ui.labelArg2.text()[:-1])
+      value = str(self.ui.lineEditArg2.text())
+      kwargs[arg] = value
+    if self.numOfArgs > 2:
+      arg = str(self.ui.labelArg3.text()[:-1])
+      value = str(self.ui.lineEditArg3.text())
+      kwargs[arg] = value
+    if self.numOfArgs > 3:
+      arg = str(self.ui.labelArg4.text()[:-1])
+      value = str(self.ui.lineEditArg4.text())
+      kwargs[arg] = value
+    if self.numOfArgs > 4:
+      arg = str(self.ui.labelArg5.text()[:-1])
+      value = str(self.ui.lineEditArg5.text())
+      kwargs[arg] = value
+
+    command = str(self.ui.listWidgetCommands.currentItem().text())
+    IP = self.IP
+    device = str(self.ui.listWidgetDevices.currentItem().text())
+    GPIB = self.GPIBlist[self.deviceList.index(device)]
+
+    #update the commands arg dict so next time the most recently applied value is filled in
+    self.argDict[command].update(kwargs)
+
+    return command, [IP, GPIB, device], kwargs
+
   
 
 if __name__ == "__main__":
