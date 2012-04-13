fa51427e40bf0961f1a923658671ef94c7eaa8fd
computer <computer@computer.(none)>
2012-04-13 12:51:03 -0500

    deleting extra file

--- test-programs/__init__.py
+++
@@ -1,3 +0,0 @@
-import getvoltageDC 
-import getvoltageAC 
-import getcurrentDC 
--- winsetup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python 
-
-
-from distutils.core import setup
-import py2exe
-
-setup(console=['deathray.py'])
